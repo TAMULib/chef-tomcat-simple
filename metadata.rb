4df7f468ed6ae46efccf75c836341d42fd8ab393
Douglas Hahn <dhahn@tamu.edu>
2016-11-02 16:19:59 -0500

    Add URI Encoding Parameter to the server.xml template.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'helpdesk@library.tamu.edu'
 license          'All rights reserved'
 description      'Installs/Configures lib-tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
 depends          'ark', '<=1.0.1'
 depends          'java', '<=1.39.0'
